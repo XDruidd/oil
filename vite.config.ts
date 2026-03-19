f5aa1f8e773fdfbc71cc08785b825cb7ccc8668e
XDruidd <137764566+XDruidd@users.noreply.github.com>
2026-03-19 18:51:12 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA, type ManifestOptions} from "vite-plugin-pwa"
+
+const manifest : Partial<ManifestOptions> | false = {
+  "theme_color":"#ffffff",
+  "background_color":"#ffffff",
+  "icons":[
+    {
+      "purpose":"maskable",
+      "sizes":"512x512",
+      "src":"icon512_maskable.png",
+      "type":"image/png"
+    },
+    {
+      "purpose":"any",
+      "sizes":"512x512",
+      "src":"icon512_rounded.png",
+      "type":"image/png"
+    }
+  ],
+  "orientation":"any",
+  "display":"standalone",
+  "lang":"en",
+  "name":"Oil",
+  "short_name":"Oil"
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/pwa-experiment/',
+  plugins: [
+    react(), 
+    VitePWA(
+      {
+        registerType: "autoUpdate",
+        workbox: {
+          globPatterns: ["**/*{html,css,js,icon,svg}"],
+        },
+        manifest: manifest
+      })],
+})
